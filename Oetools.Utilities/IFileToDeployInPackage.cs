7d27ed5dd5866bda5dbf35718742c80e263415b4
Julien Caillon <julien.caillon@gmail.com>
2018-06-09 14:43:56 +0200

    not working yet

---
+++ Oetools.Utilities/IFileToDeployInPackage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oetools.Utilities
+{
+    public interface IFileToDeployInPackage
+    {
+
+        /// <summary>
+        ///     Need to deploy this file FROM this path
+        /// </summary>
+        string From { get; set; }
+
+        /// <summary>
+        ///     Path to the pack in which we need to include this file
+        /// </summary>
+        string PackPath { get; set; }
+
+        /// <summary>
+        ///     The relative path of the file within the pack
+        /// </summary>
+        string RelativePathInPack { get; set; }
+    }
+}
