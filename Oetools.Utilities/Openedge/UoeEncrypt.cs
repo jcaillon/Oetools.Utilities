99fa038a9c5635260cec0e2c9b1ca544c214dce8
Julien Caillon <julien.caillon@gmail.com>
2018-10-05 19:47:40 +0200

    Added a port of XCODE (encrypt) and ENCODE (hash)

---
+++ Oetools.Utilities/Openedge/UoeEncrypt.cs
@@ -0,0 +1,130 @@
+#region header
+// ========================================================================
+// Copyright (c) 2018 - Julien Caillon (julien.caillon@gmail.com)
+// This file (UoeXcode.cs) is part of Oetools.Utilities.
+// 
+// Oetools.Utilities is a free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Oetools.Utilities is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Oetools.Utilities. If not, see <http://www.gnu.org/licenses/>.
+// ========================================================================
+#endregion
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Oetools.Utilities.Openedge {
+        
+    /// <summary>
+    /// Implementation of the xcode utility of openedge
+    /// The default key used by the tool XCODE is <see cref="DefaultXcodeKey"/>
+    /// The key must be in ASCII and the maximum length used is 8 (everything above 8 is just ignored)
+    /// An encrypted file is 1 byte longer than the decrypted one
+    /// A file is encrypted if it starts with the byte 0x13
+    /// </summary>
+    public class UoeEncrypt {
+        
+        private const string DefaultXcodeKey = "Progress";
+        private const short MaxKey = 15;
+        
+        private readonly ushort[] _lookupTable;
+        private readonly byte[] _keyData;
+
+        public UoeEncrypt(string key) {
+            _lookupTable = GetConstantLookupTable();
+            _keyData = GetKeyData(key);
+        }
+
+        public bool IsFileEncrypted(string filePath) {
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, 1)) {
+                var firstByte = stream.ReadByte();
+                return IsFirstByteFromEncryptedFile(firstByte);
+            }
+        }
+
+        public void ConvertFile(string filePath, bool encode, string outputFilePath) {
+            File.WriteAllBytes(outputFilePath, ConvertData(File.ReadAllBytes(filePath), encode));
+        }
+        
+        public byte[] ConvertData(byte[] data, bool encode) {
+            
+            if (data == null || data.Length == 0) {
+                return data;
+            }
+            
+            var isEncoded = IsFirstByteFromEncryptedFile(data[0]);
+            
+            if (encode && isEncoded || !encode && !isEncoded) {
+                throw new Exception($"The data is already {(encode ? "encoded" : "decoded")}");
+            }
+
+            var keyData = _keyData.ToArray();
+            
+            int idx = 0;
+            var convertedBytes = new byte[data.Length + 1];
+            var crcValue = (ushort) (data[0] == 0x11 ? 0x0025 : 0x7fed);
+            
+            if (encode) {
+                convertedBytes[0] = 0x13;
+            }
+            
+            for (int i = encode ? 0 : 1; i < data.Length; i++) {
+                if (data[i] != 13 || !encode) {
+                    var base1 = (byte) (idx++ & MaxKey);
+                    var keyValue = keyData[(base1 + keyData[MaxKey - base1]) & MaxKey];
+                    convertedBytes[idx] = (byte)(data[i] ^ keyValue);
+                    crcValue = (ushort) (_lookupTable[(encode ? data[i] : convertedBytes[idx]) & byte.MaxValue] ^ _lookupTable[crcValue & byte.MaxValue] ^ ((crcValue >> 8) & byte.MaxValue));
+                    crcValue = (ushort) (_lookupTable[keyValue & byte.MaxValue] ^ _lookupTable[crcValue & byte.MaxValue] ^ ((crcValue >> 8) & byte.MaxValue));
+                    keyData[base1] = (byte) (crcValue & byte.MaxValue);
+                }
+            }
+            
+            var output = new byte[encode ? idx + 1 : idx];
+            Array.Copy(convertedBytes, encode ? 0 : 1, output, 0, output.Length);
+            return output;
+        }
+        
+        public static ushort[] GetConstantLookupTable() {
+            var constantsTable = new ushort[256];
+            for (int j = 0; j < constantsTable.Length; j++) {
+                for (short k = 1, l = 0xC0; k < byte.MaxValue ; k <<= 1, l <<= 1) {
+                    if ((j & k) != 0) {
+                        constantsTable[j] ^= (ushort) (0xC001 ^ l);
+                    }
+                }
+            }
+            return constantsTable;
+        }
+        
+        private byte[] GetKeyData(string key) {
+            key = key ?? DefaultXcodeKey;
+            var outputKeyBytes = new byte[MaxKey + 1];
+            
+            var keyBytes = new byte[8];
+            var stringBytes = Encoding.ASCII.GetBytes(key);
+            Array.Copy(stringBytes, keyBytes, stringBytes.Length > 8 ? 8 : stringBytes.Length);
+            
+            for (int i = 0; i < keyBytes.Length; i++) {
+                int offset = (i - keyBytes[3]) & 7;
+                outputKeyBytes[offset] = (byte)(keyBytes[i] == 0 ? 17 + offset : keyBytes[i] + i);
+                outputKeyBytes[i + 8] = (byte)(outputKeyBytes[offset] ^ keyBytes[(i - 5) & 7]);
+            }
+            
+            return outputKeyBytes;
+        }
+        
+        private static bool IsFirstByteFromEncryptedFile(int firstByte) => firstByte == 0x13;
+        
+    }
+}
+
---
+++ Oetools.Utilities/Openedge/UoeHash.cs
@@ -0,0 +1,89 @@
+#region header
+
+// ========================================================================
+// Copyright (c) 2018 - Julien Caillon (julien.caillon@gmail.com)
+// This file (UoeHash.cs) is part of Oetools.Utilities.
+// 
+// Oetools.Utilities is a free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Oetools.Utilities is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Oetools.Utilities. If not, see <http://www.gnu.org/licenses/>.
+// ========================================================================
+
+#endregion
+
+using System;
+using System.Text;
+
+namespace Oetools.Utilities.Openedge {
+
+    /// <summary>
+    /// Implementation (by pvginkel) of the wrongly named ENCODE function of openedge
+    /// </summary>
+    public class UoeHash {
+        
+        private static ushort[] _lookupTable;
+        
+        /// <summary>
+        /// Implementation of the wrongly named ENCODE function of openedge which is actually a hash function
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static string Hash(byte[] input) {
+            
+            if (input == null) {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            if (_lookupTable == null) {
+                _lookupTable = UoeEncrypt.GetConstantLookupTable();
+            }
+
+            byte[] scratch = new byte[16];
+
+            ushort hash = 17;
+
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < input.Length; j++) {
+                    scratch[15 - j % 16] ^= input[j];
+                }
+
+                for (int j = 0; j < 16; j += 2) {
+                    hash = Hash(scratch, hash);
+
+                    scratch[j] = (byte) (hash & 0xff);
+                    scratch[j + 1] = (byte) ((hash >> 8) & 0xff);
+                }
+            }
+
+            byte[] target = new byte[16];
+
+            for (int i = 0; i < 16; i++) {
+                byte lower = (byte) (scratch[i] & 0x7f);
+
+                if (lower >= 'A' && lower <= 'Z' || lower >= 'a' && lower <= 'z')
+                    target[i] = lower;
+                else
+                    target[i] = (byte) ((scratch[i] >> 4) + 0x61);
+            }
+
+            return Encoding.ASCII.GetString(target);
+        }
+
+        private static ushort Hash(byte[] scratch, ushort hash) {
+            for (int i = 15; i >= 0; i--) {
+                hash = (ushort) (hash >> 8 ^ _lookupTable[hash & 0xff] ^ _lookupTable[scratch[i]]);
+            }
+            return hash;
+        }
+    }
+}
