e82aab033e3ccdce5f255052474d9442c4a62140
Julien Caillon <julien.caillon@gmail.com>
2019-01-04 19:53:36 +0100

    fix for unix

--- Oetools.Utilities/Openedge/UoeConstants.cs
+++ Oetools.Utilities/Openedge/UoeConstants.cs
@@ -34,7 +34,7 @@ namespace Oetools.Utilities.Openedge {
 
         public const string ProwinWindowClass = "ProMainWin";
 
-        public const string OeDlcEnvVar = "dlc";
+        public const string OeDlcEnvVar = "DLC";
 
         public const int MaximumCharacterLength = 31990;
         public const int MaximumPropathLength = 31990;
